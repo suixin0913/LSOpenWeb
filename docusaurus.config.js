d963a6064692ecad8d62e53604a267fa5ad3aaae
赵卓斌 <zbzhao@listenai.com>
2021-06-30 12:12:35 +0800

    [Feature]update dependencies to lastest version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,8 @@ module.exports = {
         // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
-        docsRouteBasePath: [],
+        indexPages: true,
+        docsRouteBasePath: "/",
         // For Docs using Chinese, The `language` is recommended to set to:
         // ```
         language: ["en", "zh"],
